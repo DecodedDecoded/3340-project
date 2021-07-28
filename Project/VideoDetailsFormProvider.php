46d943ec3e280191f3120223c8cfb7eea2c1f1db
Donovan-Lo <73604553+Donovan-Lo@users.noreply.github.com>
2021-07-28 11:17:58 -0400

    Add inputs to upload.php

--- Project/VideoDetailsFormProvider.php
+++ Project/VideoDetailsFormProvider.php
@@ -2,8 +2,14 @@
 class VideoDetailsFormProvider{
     public function createUploadForm(){
         $fileInput = $this->createFileInput();
+        $titleInput = $this->createTitleInput();
+        $descriptionInput = $this->createDescriptionInput();
+        $privacyInput = $this->createPrivacyInput();
         return "<form action='processing.php' method='POST'>
                     $fileInput
+                    $titleInput
+                    $descriptionInput
+                    $privacyInput
                 </form>";
     }
 
@@ -11,9 +17,33 @@ class VideoDetailsFormProvider{
 
         return "<div class='form-group'>
                     <label for='exampleFormControlFile1'>Your video/photo: </label>
-                    <input type='file' class='form-control-file' id='exampleFormControlFile1' required>
+                    <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
+                </div>";
+    }
+
+    private function createTitleInput(){
+        return "<div class='form-group'>
+                    <input class='form-control' type='text' placeholder='Title' name='titeInput'>
                 </div>";
 
+        
+    }
+
+    private function createDescriptionInput(){
+        return "<div class='form-group'>
+                    <textarea class='form-control' placeholder='Description' name='descriptionInput' rows='3'></textarea>
+                </div>";   
+    }
+
+    private function createPrivacyInput(){
+        return "<div class='form-group'>
+                    <select class='form-control' name='privacyInput'>
+                        <option value='1'>Public</option>
+                        <option value='0'>Private</option>
+                    </select>
+                </div>";
+
+      
     }
 }
 ?>
