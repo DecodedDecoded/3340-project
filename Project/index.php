45d60e95b2b08e78b6560d28c4b2aaff39ce8b55
Decoded <72431760+DecodedDecoded@users.noreply.github.com>
2021-07-28 02:10:53 -0400

    Update index & styles

--- Project/index.php
+++ Project/index.php
@@ -1,11 +1,25 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Group 1 - Hoardboard</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
 <body>
-    
+
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+        test
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+        </div>
+    </div>
+
 </body>
 </html>
