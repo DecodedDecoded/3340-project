5cf1ca954921b7e2e9f6510352958fa2de09303b
Decoded <72431760+DecodedDecoded@users.noreply.github.com>
2021-07-28 07:40:11 -0400

    Moved project folder, added register page

---
+++ Project/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HoardBoard.com - Register</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="register.css">
+       
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+
+    <div class="register__container">
+        <div class="register__box">
+            <div class="register__header">
+                <!-- Website Logo -->
+                <a class="logo__container" href="index.php">
+                    <img src="imgs/VideoTubeLogo.png" title="logo" alt="Site Logo">
+                </a>
+
+                <!-- Message -->
+                <h3 class="reg__tag">Register Account</h3>
+                <span>to join the Hoard!</span>
+            </div>
+            <div class="register__form"></div>
+            <a class="register__message" href="register.php">Log In</a>
+        </div>
+    </div>
+    </body>
+</html>
