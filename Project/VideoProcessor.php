0e9c136c8aef5d98030da1b0cbedfb26b5a15190
Donovan-Lo <73604553+Donovan-Lo@users.noreply.github.com>
2021-07-28 14:35:00 -0400

    Update VideoProcessor.php to validate the media uploaded and move the file to a folder

--- Project/VideoProcessor.php
+++ Project/VideoProcessor.php
@@ -2,6 +2,8 @@
 class VideoProcessor{
 
     private $sqlcon;
+    private $sizeLimit = 500000000;
+    private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg", "png", "jpg", "jpeg");
 
     public function __construct($sqlcon){
         $this->sqlcon = $sqlcon;
@@ -16,7 +18,47 @@ class VideoProcessor{
 
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
-        echo $tempFilePath;
+        $isValidData = $this->processData($mediaData, $tempFilePath);
+
+        if(!$isValidData){
+            return false;
+        }
+
+        if(move_uploaded_file($mediaData['tmp_name'], $tempFilePath)){
+            echo "File moved successfully";
+        }
+    }
+
+    private function processData($mediaData, $filePath){
+        $mediaType = pathinfo($filePath, PATHINFO_EXTENSION);
+
+        if(!$this->isValidSize($mediaData)){
+            echo "File too large. The media cannot be more than " . $this->sizeLimit . " bytes.";
+            return false;
+        }
+        else if(!$this->isValidType($mediaType)){
+            echo "Invalid file type";
+            return false;
+        }
+        else if($this->hasError($mediaData)){
+            echo "Error code: " . $mediaData['error'];
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isValidSize($data){
+        return $data['size'] <= $this->sizeLimit;
+    }
+
+    private function isValidType($type){
+        $lowercased = strtolower($type);
+        return in_array($lowercased, $this->allowedTypes);
+    }
+
+    private function hasError($data){
+        return $data['error'] != 0;
     }
 }
 ?>
