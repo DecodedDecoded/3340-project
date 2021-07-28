5c8e335bad62a05ab281f712a412f3dd97270de7
Decoded <72431760+DecodedDecoded@users.noreply.github.com>
2021-07-27 23:46:52 -0400

    Update front page code

--- hoardboard/src/App.js
+++ hoardboard/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import MenuBar from './Home/MenuBar';
-import Sidebar from './Home/Sidebar';
+import SearchBar from './Home/SearchBar';
 import Recommended from './Home/Recommended';
 
 function App() {
     return (
         <div className="App">
             {/* Part 1: Front Page. Each of the ff are a single component, stored in their own file */}
-            {/* MenuBar */}
+            {/* Menu Bar on side */}
             <MenuBar />
 
             <div className="app__page">
-                {/* Sidebar */}
-                <Sidebar />
+                {/* Search Bar on top */}
+                <SearchBar />
+                
                 {/* Recommendations --> Featured? */}
                 <Recommended />
             </div>          
--- hoardboard/src/Home/MenuBar.js
+++ hoardboard/src/Home/MenuBar.js
@@ -7,25 +7,17 @@ import MenuIcon from '@material-ui/icons/Menu';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import Avatar from '@material-ui/core/Avatar';
 
 function MenuBar() {
     return (
-        <div className="header">
+        <div className="menubar">
             {/*Website Logo container */}
             <div className="logo__container">
                 <Logo />
             </div> 
 
             {/* User Bar container */}
-            <div className="user__bar">
-                {/* User Profile Pic Avatar */}
-                <Avatar
-                    className="avatar"
-                    src="https://scontent.fybz2-1.fna.fbcdn.net/v/t1.6435-9/117771761_1557373284465363_7300586686690174496_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=Uh-XjyFjLH4AX8vPDzb&_nc_ht=scontent.fybz2-1.fna&oh=f53a63a60bcbd2d7cd17b21a3bbe6428&oe=60FA7040"
-                    alt="Dariq A."
-                />
-
+            <div className="menu__icons">
                 {/* Menu Icon */}
                 <MenuIcon />
                 {/* User Icons */}
